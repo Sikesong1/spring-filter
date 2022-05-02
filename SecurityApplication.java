dca3d85e6839c6ff9fbe86243c482c2f473f70c8
Tutel <1227282581@qq.com>
2022-05-02 22:50:20 +0800

    Create SecurityApplication.java

---
+++ SecurityApplication.java
@@ -0,0 +1,9 @@
+@SpringBootApplication
+@ServletComponentScan
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+}
